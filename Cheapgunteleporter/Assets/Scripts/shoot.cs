751aeabeb4936bbc2137a64df38e96e87f620188
Sherazade <chehrazad.ouazzani@hotmail.fr>
2019-06-05 16:25:02 +0200

    Game v1

---
+++ Cheapgunteleporter/Assets/Scripts/shoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject bullet = Resources.Load("Bullet") as GameObject;
+            Instantiate(bullet, transform.position, transform.rotation);
+            
+        }
+    }
+}
